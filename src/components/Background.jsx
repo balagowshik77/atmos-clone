8ec5c2827e7807f0a5874583c78027db4112c65c
balagowshik <82701802+balagowshik77@users.noreply.github.com>
2024-11-05 22:52:05 +0530

    Background changes added

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,7 +6,9 @@ import * as THREE from "three";
 export const Background = () => {
   return (
     <>
-      <Environment preset="sunset" />
+      <Environment  files="../../public/img/sky.hdr"
+        background
+        backgroundBlurriness={0.5} />
       <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
         <LayerMaterial
           color={"#ffffff"}
